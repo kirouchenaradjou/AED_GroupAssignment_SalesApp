29146fe58ae38ff35047fa15b91b503594b43f6f
Your Name <gogula.s@husky.neu.edu>
2017-10-21 15:38:29 -0500

    Added cutomer directory

---
+++ Homework6/src/BusinessPackage/Customer.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+/**
+ *
+ * @author yash
+ */
+public class Customer {
+    private String firstname;
+    private String lastname;
+    private String address;
+    private int contactnumber;
+    //private Market market;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getContactnumber() {
+        return contactnumber;
+    }
+
+    public void setContactnumber(int contactnumber) {
+        this.contactnumber = contactnumber;
+    }
+
+    /*public Market getMarket() {
+        return market;
+    }
+
+    public void setMarket(Market market) {
+        this.market = market;
+    }*/
+    
+    
+}
---
+++ Homework6/src/BusinessPackage/CustomerDirectory.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author yash
+ */
+public class CustomerDirectory {
+     private ArrayList<Customer> customerlist;
+
+    public ArrayList<Customer> getCustomerlist() {
+        return customerlist;
+    }
+
+    public void setCustomerlist(ArrayList<Customer> customerlist) {
+        this.customerlist = customerlist;
+    }
+    
+    
+}

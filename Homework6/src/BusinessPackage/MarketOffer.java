21aaabc4fb2472ea5579e6cc9d15473521ba89c4
Harshith <gowlichandrakumar.h@husky.neu.edu>
2017-10-21 16:40:07 -0400

    <Adding marketoffer>

---
+++ Homework6/src/BusinessPackage/MarketOffer.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BusinessPackage;
+
+/**
+ *
+ * @author ENVY
+ */
+public class MarketOffer {
+    
+    private Market market;
+
+    public Market getMarket() {
+        return market;
+    }
+
+    public void setMarket(Market market) {
+        this.market = market;
+    }
+}
---
+++ Homework6/src/BusinessPackage/MarketOfferCatalogue.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BusinessPackage;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ENVY
+ */
+public class MarketOfferCatalogue {
+    
+    private ArrayList<MarketOffer> marketOfferDirectory;
+
+    public ArrayList<MarketOffer> getMarketOfferDirectory() {
+        return marketOfferDirectory;
+    }
+
+    public void setMarketOfferDirectory(ArrayList<MarketOffer> marketOfferDirectory) {
+        this.marketOfferDirectory = marketOfferDirectory;
+    }
+    
+}
